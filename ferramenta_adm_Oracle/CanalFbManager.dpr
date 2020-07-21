2e7992af024414617171f795bfaa2e81d21ba424
universoti <64614511+universoti@users.noreply.github.com>
2020-07-21 17:47:42 -0300

    primeiro commit
    primeiro comit

---
+++ ferramenta_adm_Oracle/CanalFbManager.dpr
@@ -0,0 +1,14 @@
+program CanalFbManager;
+
+uses
+  Vcl.Forms,
+  frmsqleditor in 'frmsqleditor.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
